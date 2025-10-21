2eee826b58b678f93224f610a57374ba50919eac
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-10-21 15:52:31 +0530

    sharedResource

---
+++ src/ProduceConsumer1/SharedResource.java
@@ -0,0 +1,45 @@
+package ProduceConsumer1;
+
+public class SharedResource {
+    private int value;
+    //avaialable marks if data is available in the shared resource or not
+    private boolean available=false;
+
+    //this will call by the producer to put items in to the shared resource
+    public synchronized void put(int value){
+
+        while(available){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+        }
+
+        this.value=value;
+        available=true;
+        notifyAll();
+
+    }
+    //this will call by the consumer to get value
+    public synchronized int get(){
+
+        while(!available){
+            try{
+                wait();
+            }catch (Exception e){
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+        }
+
+        int value=this.value;
+        available=false;
+        System.out.println(Thread.currentThread().getName()+":"+"consumed "+value);
+
+        notifyAll();
+        return value;
+    }
+
+}

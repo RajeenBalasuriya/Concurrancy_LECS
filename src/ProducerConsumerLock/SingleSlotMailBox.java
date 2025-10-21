6478c68b50200d6306c2c81c722f0268d5b9b128
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-10-21 19:13:39 +0530

    update

--- src/ProducerConsumerLock/SingleSlotMailBox.java
+++ src/ProducerConsumerLock/SingleSlotMailBox.java
@@ -13,11 +13,42 @@ public class SingleSlotMailBox implements MailBox {
 
     @Override
     public void put(Integer value){
+        try{
+            lock.lock();//aquire the lock
+            while(available){//if available true means slot is full
+                try{
+                    notEmpty.await();//producer is put WAITING on notEmpty condition
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+            //producer will come here only if the slot is empty
+            this.slot = value;
+            this.available = true;
+            notFull.signalAll();//consumers who are in WAITING on notFull condition will woken up
+        }finally {
+            lock.unlock();
+        }
 
     }
 
     @Override
-    public Integer get(){
-        return null;
-    }
-}
+    public Integer get() {
+        try {
+            lock.lock();
+            while (!available) {//if the slot is empty
+                try {
+                    notFull.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Integer value = this.slot;
+            this.available = false;
+            return value;
+
+        } finally {
+            lock.unlock();
+        }
+    }}
